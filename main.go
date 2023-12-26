a93b258b3e0279fb7fd8e1f657538a684ae0a98f
Woul Schneider <woulschneider@gmail.com>
2023-12-26 13:29:18 -0300

    configurado acesso ao mongodb, sem usar dotenv

---
+++ main.go
@@ -0,0 +1,48 @@
+//mongodbURI := os.Getenv("MONGODB_URI")
+//mongodbUsername := os.Getenv("MONGODB_USERNAME")
+//mongodbPassword := os.Getenv("MONGODB_PASSWORD")
+
+// router := gin.Default()
+
+//initMongoDB(mongodbURI, mongodbUsername, mongodbPassword)
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Patient struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://andremillet:yRIKWOn9c5Ev1SC1@cluster0.xfhbjhm.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+}
